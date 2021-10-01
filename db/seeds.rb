a9a2da059c0dde2619fe0d4a2919ec5e3d8168ec
Igor <flashmob123@gmail.com>
2021-10-02 01:03:25 +0200

    Reconfigurating
    Fixed error with mimetype gem
    (version become unmaintanable).
    Deleted yarn.lock for npm using.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 Exercise.create!([
                    { title: 'Pull ups' },
                    { title: 'Dips' },
