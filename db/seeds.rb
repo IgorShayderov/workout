a8b9c94f8be28f82a62f3e8e8dd0057021a957d3
Igor <flashmob123@gmail.com>
2021-12-19 23:42:46 +0200

    Fixed configuration(eslint, database.yml, seed)

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+User.create!({
+  name: 'Admin',
+  email: 'admin@gmail.com',
+  password: 'Qwerty123',
+  password_confirmation: 'Qwerty123',
+  admin: true
+})
 
 Exercise.create!([
                    { title: 'Pull ups' },
